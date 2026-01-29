738482dd126d0dbba7474f4e556214eab19416f8
DavsterZ <davsterz@gmail.com>
2026-01-29 12:25:53 +0100

    fix: logica nueva para poder importar POKEMON_DB

--- app/services/pokemon_service.py
+++ app/services/pokemon_service.py
@@ -6,16 +6,23 @@ from app.models.pokemon import Pokemon
 DB_PATH = "app/data/pokemon_db.json"
 
 
-def get_pokemon(name: str) -> Pokemon:
+def _load_db():
     if not os.path.exists(DB_PATH):
-        raise FileNotFoundError("La base de datos de Pokémon no se encontró.")
+        return {}
     
-    with open("app/data/pokemon_db.json") as f:
-        POKEMON_DB = json.load(f)
+    with open(DB_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
+    
 
+POKEMON_DB = _load_db()
+
+
+def get_pokemon(name: str) -> Pokemon:
     data = POKEMON_DB.get(name.lower())
 
     if not data:
+        if not POKEMON_DB:
+            raise FileNotFoundError("La base de datos de Pokémon no se ha cargado correctamente.")
         raise ValueError(f"Pokémon '{name}' no encontrado en la base de datos.")
     
     # Al pasar data, Pydantic usara los alias automaticamente
