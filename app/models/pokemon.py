b228916a1f7e6fc49293011538035e3f090602f6
DavsterZ <davsterz@gmail.com>
2026-01-29 11:53:10 +0100

    fix: aniadir campos y usar alias para json

--- app/models/pokemon.py
+++ app/models/pokemon.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 
 
@@ -11,11 +11,19 @@ class Pokemon(BaseModel):
     defense: int
     speed: int
 
+    # Stats Speciales
+    sp_attack: int = Field(alias=special_attack)
+    sp_defense: int = Field(alias=special_defense)
+
     # Informacion util para logica avanzada
     role: str = "balanced"       # sweeper, tank, support, etc.
     cost: int = 1                # costo inicial en PokeRogue (1-10)
     abilities: List[str] = []    # Habilidades posibles
+    sprite: Optional[str] = None
 
     # Campo para guardar el estado actual en una run
     current_hp_percentage: Optional[float] = 100.0
+
+    class Config:
+        populate_by_name = True
 
